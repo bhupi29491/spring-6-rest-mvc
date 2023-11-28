d8b7d72fe4c386d144f6c47fced02ffd60396b2b
Bhupendra Singh <bhupi29491@gmail.com>
2023-11-28 20:50:04 +0200

    Added test classes for Beer & Customer repositories.

---
+++ src/test/java/com/bhupi/spring6restmvc/repositories/CustomerRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.bhupi.spring6restmvc.repositories;
+
+import com.bhupi.spring6restmvc.entities.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testSaveNewCustomer() {
+        Customer customer = customerRepository.save(Customer.builder().customerName("Yuvraj Singh").build());
+
+        assertThat(customer.getCustomerId()).isNotNull();
+    }
+}

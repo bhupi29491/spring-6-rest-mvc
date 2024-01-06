59190acb35bfd9cceede0da351fd14aa8218a5d4
Bhupendra Singh <bhupi29491@gmail.com>
2024-01-06 10:33:43 +0200

    Fixed the integration test case

--- src/test/java/com/bhupi/spring6restmvc/bootstrap/BootstrapDataTest.java
+++ src/test/java/com/bhupi/spring6restmvc/bootstrap/BootstrapDataTest.java
@@ -3,15 +3,18 @@ package com.bhupi.spring6restmvc.bootstrap;
 import com.bhupi.spring6restmvc.repositories.BeerRepository;
 import com.bhupi.spring6restmvc.repositories.CustomerRepository;
 import com.bhupi.spring6restmvc.services.BeerCsvService;
+import com.bhupi.spring6restmvc.services.BeerCsvServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 @DataJpaTest
+@Import(BeerCsvServiceImpl.class)
 class BootstrapDataTest {
 
     @Autowired
@@ -34,7 +37,7 @@ class BootstrapDataTest {
     void testRun() throws Exception {
         bootstrapData.run(null);
 
-        assertThat(beerRepository.count()).isEqualTo(3);
+        assertThat(beerRepository.count()).isEqualTo(2413);
         assertThat(customerRepository.count()).isEqualTo(3);
     }
 

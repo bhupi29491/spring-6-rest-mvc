3f9c5bead9040bdbbd52e1dda07d6859b26db4c2
Bhupendra Singh <bhupi29491@gmail.com>
2024-01-09 23:27:17 +0200

    Added new method in BeerServiceJPA for page request

--- src/main/java/com/bhupi/spring6restmvc/services/BeerServiceJPA.java
+++ src/main/java/com/bhupi/spring6restmvc/services/BeerServiceJPA.java
@@ -7,6 +7,7 @@ import com.bhupi.spring6restmvc.model.BeerStyle;
 import com.bhupi.spring6restmvc.repositories.BeerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -21,11 +22,21 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class BeerServiceJPA implements BeerService {
 
+    private final static int DEFAULT_PAGE = 0;
+    private final static int DEFAULT_PAGE_SIZE = 25;
+
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
 
     @Override
-    public List<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory, Integer pageNumber, Integer pageSize) {
+    public List<BeerDTO> listBeers(String beerName,
+                                   BeerStyle beerStyle,
+                                   Boolean showInventory,
+                                   Integer pageNumber,
+                                   Integer pageSize) {
+
+        PageRequest pageRequest = buildPageRequest(pageNumber, pageSize);
+
         List<Beer> beerList;
         if (StringUtils.hasText(beerName) && beerStyle == null) {
             beerList = listBeersByName(beerName);
@@ -46,7 +57,32 @@ public class BeerServiceJPA implements BeerService {
                 .collect(Collectors.toList());
     }
 
-    private List<Beer> listBeersByNameAndStyle(String beerName, BeerStyle beerStyle) {
+    public PageRequest buildPageRequest(Integer pageNumber, Integer pageSize) {
+        int queryPageNumber;
+        int queryPageSize;
+
+        if (pageNumber != null && pageNumber > 0) {
+            queryPageNumber = pageNumber - 1;
+        } else {
+            queryPageNumber = DEFAULT_PAGE;
+        }
+
+        if (pageSize == null) {
+            queryPageSize = DEFAULT_PAGE_SIZE;
+        } else {
+            if (pageSize > 1000) {
+                queryPageSize = 1000;
+            } else {
+                queryPageSize = pageSize;
+            }
+        }
+
+        return PageRequest.of(queryPageNumber, queryPageSize);
+    }
+
+
+    private List<Beer> listBeersByNameAndStyle(String beerName,
+                                               BeerStyle beerStyle) {
         return beerRepository.findAllByBeerNameIsLikeIgnoreCaseAndBeerStyle("%" + beerName + "%", beerStyle);
     }
 

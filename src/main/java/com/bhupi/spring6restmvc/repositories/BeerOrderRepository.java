bcbbf79db6b51a2c804e9ba2f8120994a16ec10b
Bhupendra Singh <bhupi29491@gmail.com>
2024-01-14 22:30:55 +0200

    Added beer order repository and its test class

---
+++ src/main/java/com/bhupi/spring6restmvc/repositories/BeerOrderRepository.java
@@ -0,0 +1,9 @@
+package com.bhupi.spring6restmvc.repositories;
+
+import com.bhupi.spring6restmvc.entities.BeerOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BeerOrderRepository extends JpaRepository<BeerOrder, UUID> {
+}
---
+++ src/test/java/com/bhupi/spring6restmvc/repositories/BeerOrderRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.bhupi.spring6restmvc.repositories;
+
+import com.bhupi.spring6restmvc.entities.Beer;
+import com.bhupi.spring6restmvc.entities.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class BeerOrderRepositoryTest {
+
+    @Autowired
+    BeerOrderRepository beerOrderRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    Customer testCustomer;
+    Beer testBeer;
+
+    @BeforeEach
+    void setUp() {
+        testCustomer = customerRepository.findAll()
+                .get(0);
+        testBeer = beerRepository.findAll()
+                .get(0);
+    }
+
+    @Test
+    void testBeerOrder() {
+        System.out.println(beerOrderRepository.count());
+        System.out.println(customerRepository.count());
+        System.out.println(beerRepository.count());
+        System.out.println(testCustomer.getName());
+        System.out.println(testBeer.getBeerName());
+    }
+}

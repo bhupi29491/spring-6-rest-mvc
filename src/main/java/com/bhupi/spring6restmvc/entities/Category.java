d2320a3ee58c40edb2d1be6e663998e8c85b3059
Bhupendra Singh <bhupi29491@gmail.com>
2024-01-21 11:12:16 +0200

    Added many-to-many repository for Category entity

--- src/main/java/com/bhupi/spring6restmvc/entities/Category.java
+++ src/main/java/com/bhupi/spring6restmvc/entities/Category.java
@@ -8,6 +8,7 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Version;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,9 +18,11 @@ import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.type.SqlTypes;
 
 import java.sql.Timestamp;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
+@Builder
 @Getter
 @Setter
 @NoArgsConstructor
@@ -42,12 +45,35 @@ public class Category {
 
     private String description;
 
+    @Builder.Default
     @ManyToMany
     @JoinTable(name = "beer_category",
         joinColumns = @JoinColumn(name = "category_id"),
         inverseJoinColumns = @JoinColumn(name = "beer_id"))
-    private Set<Beer> beers;
+    private Set<Beer> beers = new HashSet<>();
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category category)) return false;
 
+        if (getId() != null ? !getId().equals(category.getId()) : category.getId() != null) return false;
+        if (getVersion() != null ? !getVersion().equals(category.getVersion()) : category.getVersion() != null)
+            return false;
+        if (getCreatedDate() != null ? !getCreatedDate().equals(category.getCreatedDate()) : category.getCreatedDate() != null)
+            return false;
+        if (getDescription() != null ? !getDescription().equals(category.getDescription()) : category.getDescription() != null)
+            return false;
+        return getBeers() != null ? getBeers().equals(category.getBeers()) : category.getBeers() == null;
+    }
 
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getVersion() != null ? getVersion().hashCode() : 0);
+        result = 31 * result + (getCreatedDate() != null ? getCreatedDate().hashCode() : 0);
+        result = 31 * result + (getDescription() != null ? getDescription().hashCode() : 0);
+        result = 31 * result + (getBeers() != null ? getBeers().hashCode() : 0);
+        return result;
+    }
 }
---
+++ src/main/java/com/bhupi/spring6restmvc/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.bhupi.spring6restmvc.repositories;
+
+import com.bhupi.spring6restmvc.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
